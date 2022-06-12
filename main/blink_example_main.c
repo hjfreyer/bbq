245dc74b99e065f9fbe6ccc98098252c97db301c
Hunter Freyer <hjfreyer@google.com>
2022-06-12 18:41:14 -0400

    Two probes working.

--- main/blink_example_main.c
+++ main/blink_example_main.c
@@ -33,92 +33,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/param.h>
+#include <soc/adc_channel.h>
 
-#define BLINK_GPIO 25
-#define DUTY_PERIOD 10
+static const char *TAG = "BBQ";
 
-static const char *TAG = "ADC SINGLE";
+static double s_probe1_temp_f = 0;
+static double s_probe2_temp_f = 0;
+static int s_duty = 30;
+static esp_adc_cal_characteristics_t s_adc1_chars;
+
+#define ADC1_FROM_GPIO_IMPL(gpio) ADC1_GPIO##gpio##_CHANNEL
+#define ADC1_FROM_GPIO(gpio) ADC1_FROM_GPIO_IMPL(gpio)
+
+#define ADC1_PROBE1_CHAN ADC1_FROM_GPIO(CONFIG_PROBE1_GPIO)
+#define ADC1_PROBE2_CHAN ADC1_FROM_GPIO(CONFIG_PROBE2_GPIO)
+#define ADC1_VREF_CHAN ADC1_FROM_GPIO(CONFIG_VREF_GPIO)
+
+#define FAN_CONTROL_GPIO CONFIG_FAN_CONTROL_GPIO
+
+#define PROBE_RESISTOR_OHMS CONFIG_PROBE_RESISTOR_OHMS
 
 
-static double temp_f = 0.0;
-static int duty = 5;
+// ADC Attenuation
+#define ADC_EXAMPLE_ATTEN ADC_ATTEN_DB_11
 
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
-    gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
-    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-}
-
-//ADC Channels
+// ADC Calibration
 #if CONFIG_IDF_TARGET_ESP32
-#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_6
-static const char *TAG_CH[2][10] = {{"ADC1_CH6"}, {"ADC2_CH0"}};
-#else
-#define ADC1_EXAMPLE_CHAN0          ADC1_CHANNEL_2
-static const char *TAG_CH[2][10] = {{"ADC1_CH2"}, {"ADC2_CH0"}};
-#endif
-
-//ADC Attenuation
-#define ADC_EXAMPLE_ATTEN           ADC_ATTEN_DB_11
-
-//ADC Calibration
-#if CONFIG_IDF_TARGET_ESP32
-#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_VREF
+#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_VREF
 #elif CONFIG_IDF_TARGET_ESP32S2
-#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_TP
+#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_TP
 #elif CONFIG_IDF_TARGET_ESP32C3
-#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_TP
+#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_TP
 #elif CONFIG_IDF_TARGET_ESP32S3
-#define ADC_EXAMPLE_CALI_SCHEME     ESP_ADC_CAL_VAL_EFUSE_TP_FIT
+#define ADC_EXAMPLE_CALI_SCHEME ESP_ADC_CAL_VAL_EFUSE_TP_FIT
 #endif
 
+static double STEINHART_COEFFS[] = {7.3431401e-4, 2.1574370e-4, 9.5156860e-8};
 
-//static double CONSTANTS[] = {0.0007343140544,0.0002157437229,0.0000000951568577};
-                            static double CONSTANTS[] = {7.3431401e-4,2.1574370e-4,9.5156860e-8};
-static int adc_raw[2][10];
-
-static esp_adc_cal_characteristics_t adc1_chars;
-
-static bool adc_calibration_init(void)
-{
-    esp_err_t ret;
-    bool cali_enable = false;
-
-    ret = esp_adc_cal_check_efuse(ADC_EXAMPLE_CALI_SCHEME);
-    if (ret == ESP_ERR_NOT_SUPPORTED) {
-        ESP_LOGW(TAG, "Calibration scheme not supported, skip software calibration");
-    } else if (ret == ESP_ERR_INVALID_VERSION) {
-        ESP_LOGW(TAG, "eFuse not burnt, skip software calibration");
-    } else if (ret == ESP_OK) {
-        cali_enable = true;
-        esp_adc_cal_characterize(ADC_UNIT_1, ADC_EXAMPLE_ATTEN, ADC_WIDTH_BIT_DEFAULT, 0, &adc1_chars);
-    } else {
-        ESP_LOGE(TAG, "Invalid arg");
-    }
-
-    return cali_enable;
-}
 
 void start_mdns_service()
 {
-    //initialize mDNS service
+    // initialize mDNS service
     esp_err_t err = mdns_init();
-    if (err) {
+    if (err)
+    {
         printf("MDNS Init failed: %d\n", err);
         return;
     }
 
-    //set hostname
+    // set hostname
     mdns_hostname_set("bbq");
-    //set default instance
+    // set default instance
     mdns_instance_name_set("BBQ Thing");
     mdns_service_add(NULL, "_http", "_tcp", 80, NULL, 0);
 }
-
-
 
 /* Simple HTTP Server Example
 
@@ -133,38 +101,43 @@ void start_mdns_service()
  * handlers for the web server.
  */
 
-
 /* An HTTP GET handler */
 static esp_err_t hello_get_handler(httpd_req_t *req)
 {
-    char*  buf;
+    char *buf;
     size_t buf_len;
 
     /* Get header value string length and allocate memory for length + 1,
      * extra byte for null termination */
     buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
-    if (buf_len > 1) {
+    if (buf_len > 1)
+    {
         buf = malloc(buf_len);
         /* Copy null terminated value string into buffer */
-        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
+        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK)
+        {
             ESP_LOGI(TAG, "Found header => Host: %s", buf);
         }
         free(buf);
     }
 
     buf_len = httpd_req_get_hdr_value_len(req, "Test-Header-2") + 1;
-    if (buf_len > 1) {
+    if (buf_len > 1)
+    {
         buf = malloc(buf_len);
-        if (httpd_req_get_hdr_value_str(req, "Test-Header-2", buf, buf_len) == ESP_OK) {
+        if (httpd_req_get_hdr_value_str(req, "Test-Header-2", buf, buf_len) == ESP_OK)
+        {
             ESP_LOGI(TAG, "Found header => Test-Header-2: %s", buf);
         }
         free(buf);
     }
 
     buf_len = httpd_req_get_hdr_value_len(req, "Test-Header-1") + 1;
-    if (buf_len > 1) {
+    if (buf_len > 1)
+    {
         buf = malloc(buf_len);
-        if (httpd_req_get_hdr_value_str(req, "Test-Header-1", buf, buf_len) == ESP_OK) {
+        if (httpd_req_get_hdr_value_str(req, "Test-Header-1", buf, buf_len) == ESP_OK)
+        {
             ESP_LOGI(TAG, "Found header => Test-Header-1: %s", buf);
         }
         free(buf);
@@ -173,20 +146,25 @@ static esp_err_t hello_get_handler(httpd_req_t *req)
     /* Read URL query string length and allocate memory for length + 1,
      * extra byte for null termination */
     buf_len = httpd_req_get_url_query_len(req) + 1;
-    if (buf_len > 1) {
+    if (buf_len > 1)
+    {
         buf = malloc(buf_len);
-        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK) {
+        if (httpd_req_get_url_query_str(req, buf, buf_len) == ESP_OK)
+        {
             ESP_LOGI(TAG, "Found URL query => %s", buf);
             char param[32];
             /* Get value of expected key from query string */
-            if (httpd_query_key_value(buf, "duty", param, sizeof(param)) == ESP_OK) {
+            if (httpd_query_key_value(buf, "duty", param, sizeof(param)) == ESP_OK)
+            {
                 ESP_LOGI(TAG, "Found URL query parameter => duty=%s", param);
-                duty = atoi(param);
+                s_duty = atoi(param);
             }
-            if (httpd_query_key_value(buf, "query3", param, sizeof(param)) == ESP_OK) {
+            if (httpd_query_key_value(buf, "query3", param, sizeof(param)) == ESP_OK)
+            {
                 ESP_LOGI(TAG, "Found URL query parameter => query3=%s", param);
             }
-            if (httpd_query_key_value(buf, "query2", param, sizeof(param)) == ESP_OK) {
+            if (httpd_query_key_value(buf, "query2", param, sizeof(param)) == ESP_OK)
+            {
                 ESP_LOGI(TAG, "Found URL query parameter => query2=%s", param);
             }
         }
@@ -199,30 +177,29 @@ static esp_err_t hello_get_handler(httpd_req_t *req)
 
     /* Send response with custom headers and body set as the
      * string passed in user context*/
-    const char* resp_str = (const char*) req->user_ctx;
-   char str[80];
+    const char *resp_str = (const char *)req->user_ctx;
+    char str[80];
 
-   sprintf(str, "Temp: %f\nDuty: %d", temp_f, duty);
-
+    sprintf(str, "Temp: %f\nDuty: %d", 666.0, s_duty);
 
     httpd_resp_send(req, str, HTTPD_RESP_USE_STRLEN);
 
     /* After sending the HTTP response the old HTTP request
      * headers are lost. Check if HTTP request headers can be read now. */
-    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
+    if (httpd_req_get_hdr_value_len(req, "Host") == 0)
+    {
         ESP_LOGI(TAG, "Request headers lost");
     }
     return ESP_OK;
 }
 
 static const httpd_uri_t hello = {
-    .uri       = "/hello",
-    .method    = HTTP_GET,
-    .handler   = hello_get_handler,
+    .uri = "/hello",
+    .method = HTTP_GET,
+    .handler = hello_get_handler,
     /* Let's pass response string in user
      * context to demonstrate it's usage */
-    .user_ctx  = "Hello World!"
-};
+    .user_ctx = "Hello World!"};
 
 /* An HTTP POST handler */
 static esp_err_t echo_post_handler(httpd_req_t *req)
@@ -230,11 +207,14 @@ static esp_err_t echo_post_handler(httpd_req_t *req)
     char buf[100];
     int ret, remaining = req->content_len;
 
-    while (remaining > 0) {
+    while (remaining > 0)
+    {
         /* Read the data for the request */
         if ((ret = httpd_req_recv(req, buf,
-                        MIN(remaining, sizeof(buf)))) <= 0) {
-            if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
+                                  MIN(remaining, sizeof(buf)))) <= 0)
+        {
+            if (ret == HTTPD_SOCK_ERR_TIMEOUT)
+            {
                 /* Retry receiving if timeout occurred */
                 continue;
             }
@@ -257,11 +237,10 @@ static esp_err_t echo_post_handler(httpd_req_t *req)
 }
 
 static const httpd_uri_t echo = {
-    .uri       = "/echo",
-    .method    = HTTP_POST,
-    .handler   = echo_post_handler,
-    .user_ctx  = NULL
-};
+    .uri = "/echo",
+    .method = HTTP_POST,
+    .handler = echo_post_handler,
+    .user_ctx = NULL};
 
 /* This handler allows the custom error handling functionality to be
  * tested from client side. For that, when a PUT request 0 is sent to
@@ -276,11 +255,14 @@ static const httpd_uri_t echo = {
  */
 esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
 {
-    if (strcmp("/hello", req->uri) == 0) {
+    if (strcmp("/hello", req->uri) == 0)
+    {
         httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "/hello URI is not available");
         /* Return ESP_OK to keep underlying socket open */
         return ESP_OK;
-    } else if (strcmp("/echo", req->uri) == 0) {
+    }
+    else if (strcmp("/echo", req->uri) == 0)
+    {
         httpd_resp_send_err(req, HTTPD_404_NOT_FOUND, "/echo URI is not available");
         /* Return ESP_FAIL to close underlying socket */
         return ESP_FAIL;
@@ -290,49 +272,6 @@ esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
     return ESP_FAIL;
 }
 
-/* An HTTP PUT handler. This demonstrates realtime
- * registration and deregistration of URI handlers
- */
-static esp_err_t ctrl_put_handler(httpd_req_t *req)
-{
-    char buf;
-    int ret;
-
-    if ((ret = httpd_req_recv(req, &buf, 1)) <= 0) {
-        if (ret == HTTPD_SOCK_ERR_TIMEOUT) {
-            httpd_resp_send_408(req);
-        }
-        return ESP_FAIL;
-    }
-
-    if (buf == '0') {
-        /* URI handlers can be unregistered using the uri string */
-        ESP_LOGI(TAG, "Unregistering /hello and /echo URIs");
-        httpd_unregister_uri(req->handle, "/hello");
-        httpd_unregister_uri(req->handle, "/echo");
-        /* Register the custom error handler */
-        httpd_register_err_handler(req->handle, HTTPD_404_NOT_FOUND, http_404_error_handler);
-    }
-    else {
-        ESP_LOGI(TAG, "Registering /hello and /echo URIs");
-        httpd_register_uri_handler(req->handle, &hello);
-        httpd_register_uri_handler(req->handle, &echo);
-        /* Unregister custom error handler */
-        httpd_register_err_handler(req->handle, HTTPD_404_NOT_FOUND, NULL);
-    }
-
-    /* Respond with empty body */
-    httpd_resp_send(req, NULL, 0);
-    return ESP_OK;
-}
-
-static const httpd_uri_t ctrl = {
-    .uri       = "/ctrl",
-    .method    = HTTP_PUT,
-    .handler   = ctrl_put_handler,
-    .user_ctx  = NULL
-};
-
 static httpd_handle_t start_webserver(void)
 {
     httpd_handle_t server = NULL;
@@ -341,15 +280,15 @@ static httpd_handle_t start_webserver(void)
 
     // Start the httpd server
     ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
-    if (httpd_start(&server, &config) == ESP_OK) {
+    if (httpd_start(&server, &config) == ESP_OK)
+    {
         // Set URI handlers
         ESP_LOGI(TAG, "Registering URI handlers");
         httpd_register_uri_handler(server, &hello);
         httpd_register_uri_handler(server, &echo);
-        httpd_register_uri_handler(server, &ctrl);
-        #if CONFIG_EXAMPLE_BASIC_AUTH
+#if CONFIG_EXAMPLE_BASIC_AUTH
         httpd_register_basic_auth(server);
-        #endif
+#endif
         return server;
     }
 
@@ -363,32 +302,48 @@ static void stop_webserver(httpd_handle_t server)
     httpd_stop(server);
 }
 
-static void disconnect_handler(void* arg, esp_event_base_t event_base,
-                               int32_t event_id, void* event_data)
+static void disconnect_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
 {
-    httpd_handle_t* server = (httpd_handle_t*) arg;
-    if (*server) {
+    httpd_handle_t *server = (httpd_handle_t *)arg;
+    if (*server)
+    {
         ESP_LOGI(TAG, "Stopping webserver");
         stop_webserver(*server);
         *server = NULL;
     }
 }
 
-static void connect_handler(void* arg, esp_event_base_t event_base,
-                            int32_t event_id, void* event_data)
+static void connect_handler(void *arg, esp_event_base_t event_base,
+                            int32_t event_id, void *event_data)
 {
-    httpd_handle_t* server = (httpd_handle_t*) arg;
-    if (*server == NULL) {
+    httpd_handle_t *server = (httpd_handle_t *)arg;
+    if (*server == NULL)
+    {
         ESP_LOGI(TAG, "Starting webserver");
         *server = start_webserver();
     }
 }
 
-void app_main(void)
+static double probe_temp_f(double probe_mv, double ref_mv)
 {
-    configure_led();
-    static httpd_handle_t server = NULL;
+    // V = Vref * 2
 
+    // V = Vprobe + Vresistor
+    // Vresistor = V - VProbe
+    // Vprobe / Rprobe = Vresistor / Rresistor
+    // Rprobe = Vprobe * Rresistor / Vresistor
+    // Rprobe = Vprobe * PROBE_RESISTOR_OHMS / (Vref*2 - Vprobe)
+    double r_probe_ohm = probe_mv * PROBE_RESISTOR_OHMS / (ref_mv * 2 - probe_mv);
+
+    double log_r = log(r_probe_ohm);
+    double temp_k = 1.0 / (STEINHART_COEFFS[0] + STEINHART_COEFFS[1] * log_r + STEINHART_COEFFS[2] * log_r * log_r * log_r);
+    return (temp_k - 273.15) * 9 / 5 + 32;
+}
+
+static void init(void)
+{
+    // Network.
 
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
@@ -400,8 +355,9 @@ void app_main(void)
      */
     ESP_ERROR_CHECK(example_connect());
 
-
     start_mdns_service();
+
+    static httpd_handle_t server = NULL;
 
     ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &connect_handler, &server));
     ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
@@ -409,46 +365,45 @@ void app_main(void)
     /* Start the server for the first time */
     server = start_webserver();
 
-    esp_err_t ret = ESP_OK;
-    uint32_t voltage = 0;
-    bool cali_enable = adc_calibration_init();
+    // Fan control.
+    gpio_reset_pin(FAN_CONTROL_GPIO);
+    gpio_set_direction(FAN_CONTROL_GPIO, GPIO_MODE_OUTPUT);
 
-    //ADC1 config
+    // ADC
+    esp_adc_cal_characterize(ADC_UNIT_1, ADC_EXAMPLE_ATTEN, ADC_WIDTH_BIT_DEFAULT, 0, &s_adc1_chars);
     ESP_ERROR_CHECK(adc1_config_width(ADC_WIDTH_BIT_DEFAULT));
-    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_EXAMPLE_CHAN0, ADC_EXAMPLE_ATTEN));
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_PROBE1_CHAN, ADC_EXAMPLE_ATTEN));
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_PROBE2_CHAN, ADC_EXAMPLE_ATTEN));
+    ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_VREF_CHAN, ADC_EXAMPLE_ATTEN));
+}
+
+void app_main(void)
+{
+    init();
 
     int tick = 0;
-    while (1) {
+    while (1)
+    {
         /* Set the GPIO level according to the state (LOW or HIGH)*/
-        bool set_fan = tick % DUTY_PERIOD < duty;
-        gpio_set_level(BLINK_GPIO, set_fan);
-        ESP_LOGI(TAG, "setting fan: %d, duty = %d", (int)set_fan, duty);
+        bool set_fan = tick % CONFIG_DUTY_PERIOD < s_duty;
+        gpio_set_level(FAN_CONTROL_GPIO, set_fan);
+        ESP_LOGI(TAG, "setting fan: %d, duty = %d", (int)set_fan, s_duty);
 
+        int probe1_raw = adc1_get_raw(ADC1_PROBE1_CHAN);
+        int probe2_raw = adc1_get_raw(ADC1_PROBE2_CHAN);
+        int vref_raw = adc1_get_raw(ADC1_VREF_CHAN);
 
-        adc_raw[0][0] = adc1_get_raw(ADC1_EXAMPLE_CHAN0);
-        // ESP_LOGI(TAG_CH[0][0], "raw  data: %d", adc_raw[0][0]);
-        if (cali_enable) {
-            voltage = esp_adc_cal_raw_to_voltage(adc_raw[0][0], &adc1_chars);
-            // ESP_LOGI(TAG_CH[0][0], "cali data: %d mV", voltage);
+        uint32_t probe1_voltage = esp_adc_cal_raw_to_voltage(probe1_raw, &s_adc1_chars);
+        uint32_t probe2_voltage = esp_adc_cal_raw_to_voltage(probe2_raw, &s_adc1_chars);
+        uint32_t ref_voltage = esp_adc_cal_raw_to_voltage(vref_raw, &s_adc1_chars);
 
+        double probe1_temp_f = probe_temp_f(probe1_voltage, ref_voltage);
+        double probe2_temp_f = probe_temp_f(probe2_voltage, ref_voltage);
 
-            // V = Vprobe + Vres
-            // Vres = V - VProbe
-            // Vres = 3300 - Vprobe
-            // Vprobe / Rprobe = Vres / Rres
-            // Rprobe = Vprobe * Rres / Vres 
-            // Rprobe = Vprobe * 10000 / (3000 - Vprobe)
-            double r_probe_ohm = ((double)voltage) * 10000.0 / (3300.0 - ((double)voltage));
-            // ESP_LOGI(TAG_CH[0][0], "resistance: %f ohm", r_probe_ohm);
+        ESP_LOGI(TAG, "v_p1: %d, v_p2: %d, v_ref: %d", probe1_voltage, probe2_voltage, ref_voltage);
+        ESP_LOGI(TAG, "temp1_f: %f, temp2_f: %f", probe1_temp_f, probe2_temp_f);
 
-            double log_r = log(r_probe_ohm);
-            double temp_k = 1.0/(CONSTANTS[0] + CONSTANTS[1] * log_r + CONSTANTS[2]*log_r*log_r*log_r);
-            // ESP_LOGI(TAG_CH[0][0], "temp_k: %f", temp_k);
-            temp_f = (temp_k - 273.15)* 9/5 + 32;
-            ESP_LOGI(TAG_CH[0][0], "temp_f: %f", temp_f);
-            
-        }
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        vTaskDelay(pdMS_TO_TICKS(100));
         tick += 1;
     }
 }
