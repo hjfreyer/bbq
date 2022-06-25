ef083bced196c0d70ff09bf75c5cdfcc84fe6ba2
Hunter Freyer <hjfreyer@google.com>
2022-06-25 13:53:53 -0400

    Cloud data reporting

--- main/main.c
+++ main/main.c
@@ -1,27 +1,33 @@
 // ESP32 BBQ thermometer.
 
-#include "esp_adc_cal.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <esp_adc_cal.h>
+#include <esp_eth.h>
+#include <esp_event.h>
+#include <esp_http_server.h>
+#include <esp_log.h>
+#include <esp_netif.h>
+#include <esp_system.h>
+#include <esp_tls_crypto.h>
+#include <esp_websocket_client.h>
+#include <esp_wifi.h>
+#include <soc/adc_channel.h>
 #include <sys/param.h>
+
 #include "driver/adc.h"
-#include "esp_eth.h"
-#include "esp_log.h"
-#include "esp_netif.h"
-#include "esp_tls_crypto.h"
+#include "freertos/event_groups.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
 #include "freertos/task.h"
 #include "mdns.h"
 #include "nvs_flash.h"
 #include "protocol_examples_common.h"
-#include <esp_event.h>
-#include <esp_http_server.h>
-#include <esp_log.h>
-#include <esp_system.h>
-#include <esp_wifi.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/param.h>
-#include <soc/adc_channel.h>
+
+#include "esp_http_client.h"
+#include "esp_tls.h"
+#include "esp_crt_bundle.h"
 
 #include "state.h"
 #include "stringbuffer.h"
@@ -370,9 +376,43 @@ static void init(void)
     ESP_ERROR_CHECK(adc1_config_channel_atten(ADC1_VREF_CHAN, ADC_EXAMPLE_ATTEN));
 }
 
+void post_state() {
+
+    esp_http_client_config_t config = {
+        .url = "https://bbqbe.hjfreyer.repl.co/device",
+        .method = HTTP_METHOD_POST,
+    };
+
+    /* Send response with custom headers and body set as the
+     * string passed in user context*/
+    char resp[RESPONSE_BUFFER_SIZE];
+
+    state_json(resp, sizeof(resp), &s_state);
+
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+    esp_http_client_set_post_field(client, resp, strlen(resp));
+    esp_http_client_set_header(client, "content-type", "application/json");
+    esp_err_t err = esp_http_client_perform(client);
+
+    if (err == ESP_OK)
+    {
+        ESP_LOGI(TAG, "HTTPS Status = %d, content_length = %d",
+                 esp_http_client_get_status_code(client),
+                 esp_http_client_get_content_length(client));
+    }
+    else
+    {
+        ESP_LOGE(TAG, "Error perform http request %s", esp_err_to_name(err));
+    }
+    esp_http_client_cleanup(client);
+}
+
 void app_main(void)
 {
     init();
+    ESP_LOGI(TAG, "[APP] Startup..");
+    ESP_LOGI(TAG, "[APP] Free memory: %d bytes", esp_get_free_heap_size());
+    ESP_LOGI(TAG, "[APP] IDF version: %s", esp_get_idf_version());
 
     int tick = 0;
     while (1)
@@ -384,6 +424,11 @@ void app_main(void)
             // ESP_LOGI(TAG, "v_p1: %d, v_p2: %d, v_ref: %d", probe1_voltage, probe2_voltage, ref_voltage);
             ESP_LOGI(TAG, "temp1_f: %d, temp2_f: %d", s_state.probe1_f, s_state.probe2_f);
         }
+
+        if (tick % CONFIG_REPORT_PERIOD == 0) {
+            post_state();
+        }
+
         /* Set the GPIO level according to the state (LOW or HIGH)*/
         bool set_fan = tick % CONFIG_DUTY_PERIOD < s_state.duty_pct * CONFIG_DUTY_PERIOD / 100;
         gpio_set_level(FAN_CONTROL_GPIO, set_fan);
@@ -407,201 +452,3 @@ void app_main(void)
         tick += 1;
     }
 }
-
-// #include <string.h>
-// #include <stdlib.h>
-// #include "freertos/FreeRTOS.h"
-// #include "freertos/task.h"
-// #include "freertos/event_groups.h"
-// #include "esp_wifi.h"
-// #include "esp_event.h"
-// #include "esp_log.h"
-// #include "esp_system.h"
-// #include "nvs_flash.h"
-// #include "protocol_examples_common.h"
-// #include "esp_netif.h"
-
-// #include "lwip/err.h"
-// #include "lwip/sockets.h"
-// #include "lwip/sys.h"
-// #include "lwip/netdb.h"
-// #include "lwip/dns.h"
-
-// #include "esp_tls.h"
-// #include "esp_crt_bundle.h"
-
-// /* Constants that aren't configurable in menuconfig */
-// #define WEB_SERVER "www.howsmyssl.com"
-// #define WEB_PORT "443"
-// #define WEB_URL "https://www.howsmyssl.com/a/check"
-
-// static const char *TAG = "example";
-
-// static const char REQUEST[] = "GET " WEB_URL " HTTP/1.1\r\n"
-//                              "Host: "WEB_SERVER"\r\n"
-//                              "User-Agent: esp-idf/1.0 esp32\r\n"
-//                              "\r\n";
-
-// /* Root cert for howsmyssl.com, taken from server_root_cert.pem
-
-//    The PEM file was extracted from the output of this command:
-//    openssl s_client -showcerts -connect www.howsmyssl.com:443 </dev/null
-
-//    The CA root cert is the last cert given in the chain of certs.
-
-//    To embed it in the app binary, the PEM file is named
-//    in the component.mk COMPONENT_EMBED_TXTFILES variable.
-// */
-// extern const uint8_t server_root_cert_pem_start[] asm("_binary_server_root_cert_pem_start");
-// extern const uint8_t server_root_cert_pem_end[]   asm("_binary_server_root_cert_pem_end");
-// #ifdef CONFIG_ESP_TLS_CLIENT_SESSION_TICKETS
-// esp_tls_client_session_t *tls_client_session = NULL;
-// #endif
-// static void https_get_request(esp_tls_cfg_t cfg)
-// {
-//     char buf[512];
-//     int ret, len;
-
-//     struct esp_tls *tls = esp_tls_conn_http_new(WEB_URL, &cfg);
-
-//     if (tls != NULL) {
-//         ESP_LOGI(TAG, "Connection established...");
-//     } else {
-//         ESP_LOGE(TAG, "Connection failed...");
-//         goto exit;
-//     }
-
-// #ifdef CONFIG_ESP_TLS_CLIENT_SESSION_TICKETS
-//     /* The TLS session is successfully established, now saving the session ctx for reuse */
-//     if (tls_client_session == NULL) {
-//         tls_client_session = esp_tls_get_client_session(tls);
-//     }
-// #endif
-//     size_t written_bytes = 0;
-//     do {
-//         ret = esp_tls_conn_write(tls,
-//                                  REQUEST + written_bytes,
-//                                  sizeof(REQUEST) - written_bytes);
-//         if (ret >= 0) {
-//             ESP_LOGI(TAG, "%d bytes written", ret);
-//             written_bytes += ret;
-//         } else if (ret != ESP_TLS_ERR_SSL_WANT_READ  && ret != ESP_TLS_ERR_SSL_WANT_WRITE) {
-//             ESP_LOGE(TAG, "esp_tls_conn_write  returned: [0x%02X](%s)", ret, esp_err_to_name(ret));
-//             goto exit;
-//         }
-//     } while (written_bytes < sizeof(REQUEST));
-
-//     ESP_LOGI(TAG, "Reading HTTP response...");
-
-//     do {
-//         len = sizeof(buf) - 1;
-//         bzero(buf, sizeof(buf));
-//         ret = esp_tls_conn_read(tls, (char *)buf, len);
-
-//         if (ret == ESP_TLS_ERR_SSL_WANT_WRITE  || ret == ESP_TLS_ERR_SSL_WANT_READ) {
-//             continue;
-//         }
-
-//         if (ret < 0) {
-//             ESP_LOGE(TAG, "esp_tls_conn_read  returned [-0x%02X](%s)", -ret, esp_err_to_name(ret));
-//             break;
-//         }
-
-//         if (ret == 0) {
-//             ESP_LOGI(TAG, "connection closed");
-//             break;
-//         }
-
-//         len = ret;
-//         ESP_LOGD(TAG, "%d bytes read", len);
-//         /* Print response directly to stdout as it is read */
-//         for (int i = 0; i < len; i++) {
-//             putchar(buf[i]);
-//         }
-//         putchar('\n'); // JSON output doesn't have a newline at end
-//     } while (1);
-
-// exit:
-//     esp_tls_conn_delete(tls);
-//     for (int countdown = 10; countdown >= 0; countdown--) {
-//         ESP_LOGI(TAG, "%d...", countdown);
-//         vTaskDelay(1000 / portTICK_PERIOD_MS);
-//     }
-// }
-
-// static void https_get_request_using_crt_bundle(void)
-// {
-//     ESP_LOGI(TAG, "https_request using crt bundle");
-//     esp_tls_cfg_t cfg = {
-//         .crt_bundle_attach = esp_crt_bundle_attach,
-//     };
-//     https_get_request(cfg);
-// }
-
-// static void https_get_request_using_cacert_buf(void)
-// {
-//     ESP_LOGI(TAG, "https_request using cacert_buf");
-//     esp_tls_cfg_t cfg = {
-//         .cacert_buf = (const unsigned char *) server_root_cert_pem_start,
-//         .cacert_bytes = server_root_cert_pem_end - server_root_cert_pem_start,
-//     };
-//     https_get_request(cfg);
-// }
-
-// static void https_get_request_using_global_ca_store(void)
-// {
-//     esp_err_t esp_ret = ESP_FAIL;
-//     ESP_LOGI(TAG, "https_request using global ca_store");
-//     esp_ret = esp_tls_set_global_ca_store(server_root_cert_pem_start, server_root_cert_pem_end - server_root_cert_pem_start);
-//     if (esp_ret != ESP_OK) {
-//         ESP_LOGE(TAG, "Error in setting the global ca store: [%02X] (%s),could not complete the https_request using global_ca_store", esp_ret, esp_err_to_name(esp_ret));
-//         return;
-//     }
-//     esp_tls_cfg_t cfg = {
-//         .use_global_ca_store = true,
-//     };
-//     https_get_request(cfg);
-//     esp_tls_free_global_ca_store();
-// }
-
-// #ifdef CONFIG_ESP_TLS_CLIENT_SESSION_TICKETS
-// static void https_get_request_using_already_saved_session(void)
-// {
-//     ESP_LOGI(TAG, "https_request using saved client session");
-//     esp_tls_cfg_t cfg = {
-//         .client_session = tls_client_session,
-//     };
-//     https_get_request(cfg);
-//     free(tls_client_session);
-//     tls_client_session = NULL;
-// }
-// #endif
-
-// static void https_request_task(void *pvparameters)
-// {
-//     ESP_LOGI(TAG, "Start https_request example");
-
-//     https_get_request_using_crt_bundle();
-//     https_get_request_using_cacert_buf();
-//     https_get_request_using_global_ca_store();
-// #ifdef CONFIG_ESP_TLS_CLIENT_SESSION_TICKETS
-//     https_get_request_using_already_saved_session();
-// #endif
-//     ESP_LOGI(TAG, "Finish https_request example");
-//     vTaskDelete(NULL);
-// }
-
-// void app_main(void)
-// {
-//     ESP_ERROR_CHECK( nvs_flash_init() );
-//     ESP_ERROR_CHECK(esp_netif_init());
-//     ESP_ERROR_CHECK(esp_event_loop_create_default());
-
-//     /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-//      * Read "Establishing Wi-Fi or Ethernet Connection" section in
-//      * examples/protocols/README.md for more information about this function.
-//      */
-//     ESP_ERROR_CHECK(example_connect());
-
-//     xTaskCreate(&https_request_task, "https_get_task", 8192, NULL, 5, NULL);
-// }
