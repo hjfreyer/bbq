5876711fa30b74f9eafe99a374f20a29757e73f2
Hunter Freyer <yt@hjfreyer.com>
2024-04-01 12:49:15 +0000

    Store telemetry in DB

--- bbqfe/app/api/readings/route.ts
+++ bbqfe/app/api/readings/route.ts
@@ -1,10 +1,49 @@
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
- 
-export function GET(request: Request) {
-  return new Response(`Hello from ${process.env.FOO}`);
+
+import admin from 'firebase-admin';
+import { FieldValue } from 'firebase-admin/firestore';
+
+
+if (admin.apps.length === 0) {
+  if (process.env.GCP_CREDS) {
+    admin.initializeApp({
+      credential: admin.credential.cert(JSON.parse(process.env.GCP_CREDS))
+    });
+
+  } else if (process.env.BBQ_FIREBASE_SERVICE_ACCOUNT_KEY) {
+    admin.initializeApp({
+      credential: admin.credential.cert(JSON.parse(process.env.BBQ_FIREBASE_SERVICE_ACCOUNT_KEY))
+    });
+  }
 }
 
+let db = admin.firestore();
+
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
+
+type Readings = {
+  session: number,
+  food_temp_f: number,
+  ambient_temp_f: number,
+  duty_pct: number,
+};
+
 export async function POST(request: Request) {
-  console.log(await request.json());
-  return new Response(`POSTED`);
+  const readings = await request.json() as Readings;
+
+  const session_ref = db.collection("sessions").doc("" + readings.session);
+  const now = new Date();
+
+  session_ref.set(
+    {
+      last_update: now,
+      samples: FieldValue.arrayUnion({
+        time: now,
+        duty_pct: readings.duty_pct,
+        ambient_temp_f: readings.ambient_temp_f,
+        food_temp_f: readings.food_temp_f,
+      }),
+    },
+    { merge: true }
+  );
+  return new Response(`ok\n`);
 }
