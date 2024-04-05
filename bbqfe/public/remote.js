46e296a67797b6a68b0aa966d95524ea8b8aef2a
Hunter Freyer <yt@hjfreyer.com>
2024-04-05 21:41:47 +0000

    Test remote UI.

--- bbqfe/public/remote.js
+++ bbqfe/public/remote.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+
+const css = document.createElement("link");
+css.rel = "stylesheet";
+css.href = "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css";
+css.integrity = "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH";
+css.crossOrigin = "anonymous";
+document.head.appendChild(css);
+
+const script = document.createElement("script");
+script.type = "module";
+script.src = '/remotemain.js';
+document.head.appendChild(script)
---
+++ bbqfe/public/remotemain.js
@@ -0,0 +1,37 @@
+
+import 'https://code.jquery.com/jquery-3.7.1.min.js';
+
+
+
+$(() => {
+  const bodyHtml = `
+    <body>
+    <div class="container my-5">
+      <h1>BBQ Remote</h1>
+      <div class="col-lg-8 px-0">
+        <p>Food temp: <span id="food-temp"></span></p>
+        <p class="fs-5">You've successfully loaded up the Bootstrap starter example. It includes <a href="https://getbootstrap.com/">Bootstrap 5</a> via the <a href="https://www.jsdelivr.com/package/npm/bootstrap">jsDelivr CDN</a> and includes an additional CSS and JS file for your own code.</p>
+        <p>Feel free to download or copy-and-paste any parts of this example.</p>
+
+        <hr class="col-1 my-4">
+
+        <a href="https://getbootstrap.com" class="btn btn-primary">Read the Bootstrap docs</a>
+        <a href="https://github.com/twbs/examples" class="btn btn-secondary">View on GitHub</a>
+      </div>
+    </div>
+  </body>`;
+
+  document.body.innerHTML = bodyHtml;
+
+  const foodTemp = $('#food-temp').text('ahoy');
+
+  async function update() {
+    const result = await fetch('http://bbq.local/settings');
+
+    const json = await result.json();
+
+    foodTemp.text(json['threshold_f'])
+  }
+
+  update().then(() => console.log("done"))
+})
